ea7dee3653ddca4303eff1166b2e00af0f4b25fb
Ellvius <lishinvs6541@gmail.com>
2025-07-01 23:49:33 +0530

    Use mapped columns in models to improve type checking

--- backend/app/models/users.py
+++ backend/app/models/users.py
@@ -1,14 +1,19 @@
-from sqlalchemy import Column, Integer, String
-from sqlalchemy.orm import relationship
+from typing import List
+from sqlalchemy import Integer, String
+from sqlalchemy.orm import relationship, mapped_column, Mapped
 from app.core.db import Base
 from app.schemas.user_schema import UserRoles
 
 class User(Base):
     __tablename__ = "users"
     
-    id = Column(Integer, primary_key=True, index=True) 
-    username = Column(String, index=True, unique=True)
-    password = Column(String)
-    role = Column(String, default=UserRoles.USER.value, nullable=False)
+    id: Mapped[int] = mapped_column(Integer, primary_key=True, index=True) 
+    username: Mapped[str] = mapped_column(String, index=True, unique=True)
+    password: Mapped[str] = mapped_column(String)
+    role:Mapped[str] = mapped_column(String, default=UserRoles.USER.value, nullable=False)
     
-    containers = relationship("Container", back_populates="owner", cascade="all, delete")
+    containers: Mapped[List["Container"]] = relationship( # type: ignore
+        "Container",
+        back_populates="owner",
+        cascade="all, delete-orphan"
+    )
