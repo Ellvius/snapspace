f64023d6d17d4615dea74e0462cdcf39dedb340b
Ellvius <lishinvs6541@gmail.com>
2025-07-21 01:21:32 +0530

    Initialize react frontend

---
+++ frontend/src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+const App = () => {
+  return (
+    <>
+        <h1 className='text-3xl font-bold underline'>
+            Snapspace
+        </h1>
+    </>
+  )
+}
+
+export default App;
